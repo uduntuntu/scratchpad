a9ef4cac9c8706615f3ad07a08d07a12989778e4
Utu Hopiavuori <git@mail.hopiavuori.net>
2025-10-02 08:56:41 +0300

    laptop updates

--- bash/wallpapers-randomizer.sh
+++ bash/wallpapers-randomizer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Select 3 random pictures from folder (as many as displays)
-WALLPAPERS=$(find /usr/share/backgrounds/gnome/ -type f | shuf -n 3)
+WALLPAPERS=$(find /usr/share/wallpapers -type f | shuf -n 3)
 
 # set background pictures for each display
 feh --bg-fill $WALLPAPERS
